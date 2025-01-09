c6244cf029bd2b74a72c53a8c7f4f3e3c29aee3d
Cristian <cristian.coppari@gmail.com>
2025-01-08 21:09:42 -0300

    feature: initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,26 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "@next/next/no-img-element": "warn",
+      "import/order": [
+        "warn",
+        {
+          groups: ["type", "builtin", "object", "external", "internal", "parent", "sibling", "index"],
+          pathGroups: [
+            {
+              pattern: "~/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
