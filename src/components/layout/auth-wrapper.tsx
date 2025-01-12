a529b2173ac0d669be489ae097eb14cee8d25b5c
Cristian <cristian.coppari@gmail.com>
2025-01-12 20:41:49 -0300

    chore: remove refetchOnMount

--- src/components/layout/auth-wrapper.tsx
+++ src/components/layout/auth-wrapper.tsx
@@ -10,7 +10,6 @@ export default function AuthWrapper({ children }: PropsWithChildren) {
   const { isLoading, error } = useQuery({
     queryKey: ["api-token"],
     queryFn: getToken,
-    refetchOnMount: true,
   });
 
   if (isLoading)
