f19af657318054cdcf4216afab8b50fc75a7e99b
Cristian <cristian.coppari@gmail.com>
2025-01-12 19:18:01 -0300

    fix: type error

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -2,7 +2,7 @@
  * https://www.totaltypescript.com/how-to-strongly-type-process-env
  */
 
-namespace NodeJS {
+declare namespace NodeJS {
   interface ProcessEnv {
     IGDB_CLIENT_ID: string;
     IGDB_CLIENT_SECRET: string;
