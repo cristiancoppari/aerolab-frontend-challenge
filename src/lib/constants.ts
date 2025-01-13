1a72e9d1ac5e2e7974a75859b77953e9fa15e793
Cristian <cristian.coppari@gmail.com>
2025-01-13 00:43:26 -0300

    fix: opengraph data

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,3 @@
-import { Metadata } from "next";
-
 /**
  * IGDB BASE URL
  * https://api-docs.igdb.com/#getting-started
@@ -11,22 +9,6 @@ export const IGDB_BASE_URL = "https://api.igdb.com/v4";
 export const FADE_IN_VARIANTS = {
   hidden: { opacity: 0 },
   visible: { opacity: 1 },
-};
-
-export const OG_DATA_BASE: Metadata["openGraph"] = {
-  title: "Gaming Haven Z",
-  description:
-    "Application for searching video games and their reviews from your favorite platforms.",
-  siteName: "Gaming Haven Z",
-  locale: "en_US",
-  type: "website",
-};
-
-export const TWITTER_DATA_BASE: Metadata["twitter"] = {
-  title: OG_DATA_BASE.title,
-  description: OG_DATA_BASE.description,
-  creator: "@cristiancoppari",
-  site: "@cristiancoppari",
 };
 
 export const PROD_URL = "https://aerolab-frontend-challenge-one.vercel.app";
---
+++ src/lib/opengraph-data.ts
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+export const OG_DATA_BASE: Metadata["openGraph"] = {
+  title: "Gaming Haven Z",
+  description:
+    "Application for searching video games and their reviews from your favorite platforms.",
+  siteName: "Gaming Haven Z",
+  locale: "en_US",
+  type: "website",
+};
+
+export const TWITTER_DATA_BASE: Metadata["twitter"] = {
+  title: OG_DATA_BASE.title,
+  description: OG_DATA_BASE.description,
+  creator: "@cristiancoppari",
+  site: "@cristiancoppari",
+};
