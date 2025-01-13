48a5feebd096838da4baa9828d9e3a6c8e6ac2d7
Cristian <cristian.coppari@gmail.com>
2025-01-13 01:33:20 -0300

    chore: add loading eager to the empty image

--- src/components/collected-games.tsx
+++ src/components/collected-games.tsx
@@ -107,6 +107,7 @@ function EmptyState() {
         unoptimized
         width={358}
         height={168}
+        loading="eager"
       />
 
       <div className="mt-6 flex flex-col gap-2 text-center">
