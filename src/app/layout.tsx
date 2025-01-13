af3b236a98213d9a3eab82707b8e39fa70973c1d
Cristian <cristian.coppari@gmail.com>
2025-01-12 23:43:06 -0300

    fix: add twitter data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,16 +8,19 @@ import { GameStoreProvider } from "@/providers/local-stored-games.provider";
 import AuthWrapper from "@/components/layout/auth-wrapper";
 import { getBaseUrl } from "@/lib/utils";
 import Background from "@/components/layout/background";
-import { OG_DATA_BASE } from "@/lib/constants";
+import { OG_DATA_BASE, TWITTER_DATA_BASE } from "@/lib/constants";
 
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
-  title: OG_DATA_BASE.title,
-  description: OG_DATA_BASE.description,
+  title: OG_DATA_BASE?.title,
+  description: OG_DATA_BASE?.description,
   openGraph: {
     ...OG_DATA_BASE,
     url: getBaseUrl(),
+  },
+  twitter: {
+    ...TWITTER_DATA_BASE,
   },
 };
 
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
+import { Metadata } from "next";
+
 /**
  * IGDB BASE URL
  * https://api-docs.igdb.com/#getting-started
@@ -11,11 +13,18 @@ export const FADE_IN_VARIANTS = {
   visible: { opacity: 1 },
 };
 
-export const OG_DATA_BASE = {
+export const OG_DATA_BASE: Metadata["openGraph"] = {
   title: "Gaming Haven Z",
   description:
     "Application for searching video games and their reviews from your favorite platforms.",
   siteName: "Gaming Haven Z",
   locale: "en_US",
   type: "website",
+};
+
+export const TWITTER_DATA_BASE: Metadata["twitter"] = {
+  title: OG_DATA_BASE.title,
+  description: OG_DATA_BASE.description,
+  creator: "@cristiancoppari",
+  site: "@cristiancoppari",
 };
